98a7f6566edd09723212afb92828ebcf953fed86
Bolude Daniel <98927610+ZyhvarZeGreat@users.noreply.github.com>
2024-09-11 10:15:31 +0200

    added classification

--- src/sections/BubbleChart.jsx
+++ src/sections/BubbleChart.jsx
@@ -115,8 +115,8 @@ export function BubbleChart({ data, classification }) {
                     <img src={'https://tabodozo.sirv.com/bubble-img2.png'} className="h-14 object-contain" />
                 </div>
                 <div className="text-xl flex flex-col">
-                    <p>Overall label: {classification?.label}</p>
-                    <p>score: {classification?.score?.toFixed(2)}</p> </div>
+                    {classification && <p>Overall label: {classification?.label}</p>}
+                    {classification && <p>score: {classification?.score?.toFixed(2)}</p> </div>}
             </div>
             <CardContent className="w-auto flex-col p-0 font-graphik h-full flex justify-center items-center">
                 <div className="relative w-full h-full">
